4b124403d5cf4360481deaadcedb1f60fcda508f
CYrancher23 <77399044+CYrancher23@users.noreply.github.com>
2021-04-02 12:17:16 -0500

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import "./Header.css"
 
 
 class Header extends Component {
     render() {
         return (
-            <div>
-                <Link to= "/">Home</Link>
+            <div className="home"> 
+                <Link to= "/" className="home__link">TMDB Top Five</Link>
             </div>
         )
     }
